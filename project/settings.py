1113ee2c02f2b97009a726390735281c1a86b800
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-29 22:21:08 +0100

    add static files and main templates

--- project/settings.py
+++ project/settings.py
@@ -117,5 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "project/static",
+]
