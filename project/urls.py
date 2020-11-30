38b3a9d9a5a371e67e7be7a5195ffff57b59ea23
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-30 15:27:55 +0100

    add products url

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-        path('accounts/', include('accounts.urls'))
+        path('accounts/', include('accounts.urls')),
+                path('products/', include('products.urls'))
+
 
 ]
