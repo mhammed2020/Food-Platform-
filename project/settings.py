7f6ffe62df22a8d1921da3c3208f72190ce649ec
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-30 20:58:28 +0100

    install psycopg2

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
          'NAME': 'restauDB',
-        'USER': 'medo',
+        'USER': 'usr',
         'PASSWORD': 'pwd',
         'HOST'  : 'localhost'
     }
