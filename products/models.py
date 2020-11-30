e8f1270016bc2409ed0b774696843f3f95951be4
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-30 21:21:29 +0100

    create Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Name(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default = False)
+    published_at = models.DateTimeField(default = datetime.now)
+
+    def __str__(self):
+        return self.name
+
+  
