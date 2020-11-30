af4d2fbf6c79283a1984938eb19188d4fe3aaf1e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-30 21:33:27 +0100

    remigrate

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    photo = models.ImageField(upload_to ='photo/%Y/%m/%d/')
+    photo = models.ImageField(upload_to ='photo/%Y/%m/%d/', blank =True, null=True)
     published_at = models.DateTimeField(default = datetime.now)
 
     def __str__(self):
