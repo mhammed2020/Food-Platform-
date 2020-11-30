a5d18f7bc39c965527cc7a48f0bbbc7bd91c6ec7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-30 22:16:18 +0100

    update product view

--- products/views.py
+++ products/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from . models import Product
 
 def product_list(request):
 
+    products = Product.objects.all()
 
-    return render (request,'products/product_list.html')
+    context = {
+        'products' : products
+    }
+
+
+
+    return render (request,'products/product_list.html',context)
 
 
 def product(request):
